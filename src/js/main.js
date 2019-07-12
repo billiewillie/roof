eda0a07a39d38ddb600cacaa996d660a1555091e
Billie Willie <vadimbelinovich@gmail.com>
2019-07-12 11:03:57 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 // import * as photostack from 'photostack'
+import "slick-carousel";
 import * as burger from "./component/nav";
 import * as accordion from "./component/accordion";
+
+$(".slider").slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
