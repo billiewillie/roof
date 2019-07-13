0964e94602bc8af2611c5c2580b7bd1ef140b834
Billie Willie <vadimbelinovich@gmail.com>
2019-07-13 23:04:14 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,22 @@ $(".slider").slick({
   slidesToScroll: 1,
   autoplaySpeed: 5000
 });
+
+$(".offer__slider .list").slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplaySpeed: 5000,
+  fade: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        destroy: true
+      }
+    }
+  ]
+});
