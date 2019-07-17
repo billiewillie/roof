82e4fa3bb264ce1ae5c979a233faf8d9d7e16921
vadimbelinovich <vadimbelinovich@gmail.com>
2019-07-17 12:04:06 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,17 @@ import * as burger from "./component/nav";
 import * as scroll from "./component/scroll";
 import * as accordion from "./component/accordion";
 
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+  ym(54476875, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true
+});
+
 $(".offer__list").slick({
   dots: false,
   arrows: true,
